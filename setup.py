416d4ec2926f160cb1033769a7fb4431b5b984ed
Musab2077 <muhammadmusab2077@gmail.com>
2025-02-02 17:34:12 +0500

    Setup Ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MEdical LLM Project',
+    version = '0.0.0',
+    author = 'Muhammad Musab',
+    author_email = 'muhammadmusab2077@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
